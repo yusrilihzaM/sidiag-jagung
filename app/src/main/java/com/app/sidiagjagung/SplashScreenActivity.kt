254ec08903d3e38cfe59cd32181afd975ab68551
Moh Yusril Ihza Maulana <mohyusrilihzamaulana@gmail.com>
2021-10-22 11:44:59 +0700

    splash screen done

--- app/src/main/java/com/app/sidiagjagung/SplashScreenActivity.kt
+++ app/src/main/java/com/app/sidiagjagung/SplashScreenActivity.kt
@@ -1,12 +1,25 @@
 package com.app.sidiagjagung
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreenActivity : Activity() {
+    companion object{
+        const val TIME:Long=3000
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({ moveActivity() }, TIME)
+    }
+
+    private fun moveActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
